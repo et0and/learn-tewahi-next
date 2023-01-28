532b97a2c7c4425737f28839ab263915a0d94c40
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-01-28 16:11:28 +1300

    Check this works

--- theme.config.tsx
+++ theme.config.tsx
@@ -18,6 +18,7 @@ export default {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content="Te Wāhi Auaha" />
         <meta property="og:description" content="A learning hub." />
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
       </>
     ),
 }
