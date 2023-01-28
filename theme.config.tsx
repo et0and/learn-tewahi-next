b8de99e4328363f368f054519970eab0082e86fd
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-01-28 15:03:14 +1300

    N

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,3 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-
-const config: DocsThemeConfig = {
-  logo: <span>Te Wāhi Learn</span>,
-  docsRepositoryBase: 'https://tewahi.com',
-  footer: {
-    text: 'Te Wāhi Auaha, 2023',
-  },
-}
-
 export default {
   head: (
     <>
