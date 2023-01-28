80c9e47a6c818fb37c99e44b7386a7d5e5155ff7
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-01-28 15:01:46 +1300

    Push

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span>Te Wāhi Learn</span>,
@@ -11,13 +10,11 @@ const config: DocsThemeConfig = {
 }
 
 export default {
-  head: () => {
-    const { asPath } = useRouter()
-    const { frontMatter } = useConfig()
-    return <>
-      <meta property="og:url" content={`https://my-app.com${asPath}`} />
-      <meta property="og:title" content={frontMatter.title || 'Te Wāhi Learn'} />
-      <meta property="og:description" content={frontMatter.description || 'Te Wāhi Learn'} />
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Te Wāhi Auaha" />
+      <meta property="og:description" content="A learning hub" />
     </>
-  },
+  ),
 }
