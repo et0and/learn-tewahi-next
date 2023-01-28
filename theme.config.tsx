738712e6bb63a55354232f14fa7ea9737f4875de
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-01-28 14:17:59 +1300

    First commit on new Nextra instance

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Te Wāhi Learn</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
