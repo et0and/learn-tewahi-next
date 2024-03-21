4f1bf9633fb5fdb598fcc06361dc1c5b39757b05
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-03-21 18:56:26 +1300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -51,10 +51,8 @@ export default {
         )}`
       }
     },
-    editLink: {
-      text: '',
-    },
-  editLink.component: null,
+    
+  
     head: (
       <>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
