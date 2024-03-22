6cb008288008f8ab7653ccf7ea2270d757ccde0c
Tom Hackshaw <gh@tomhackshaw.com>
2024-03-22 17:41:31 +1300

    Hmm

--- pages/tools/other/[filename].js
+++ pages/tools/other/[filename].js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import { useTina } from 'tinacms/dist/react'
 import { TinaMarkdown } from 'tinacms/dist/rich-text'
-import client from '@/tina/__generated__/client'
-
+import client from '../../../tina/__generated__/client'
 
 const OtherPage = (props) => {
   const { data } = useTina({
