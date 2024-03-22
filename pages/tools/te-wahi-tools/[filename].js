3b45998c93b0f4adfd445a67351a8e2ab18c8d2e
Tom Hackshaw <gh@tomhackshaw.com>
2024-03-22 17:37:39 +1300

    This should actually do it

--- pages/tools/te-wahi-tools/[filename].js
+++ pages/tools/te-wahi-tools/[filename].js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useTina } from 'tinacms/dist/react'
 import { TinaMarkdown } from 'tinacms/dist/rich-text'
-import client from './tina/__generated__/client'
+import client from '@/tina/__generated__/client'
 
 
 const TeWahiToolsPage = (props) => {
