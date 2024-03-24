2bc338d24574c7a9ce88002c98ff31471bd403d1
et0and <gh@tomhackshaw.com>
2024-03-24 15:30:11 +1300

    Removed banner

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,39 +1,33 @@
-import { useConfig } from 'nextra-theme-docs'
+import { useConfig } from "nextra-theme-docs";
 
 export default {
-  docsRepositoryBase: 'https://github.com/et0and/learn-tewahi-next/tree/main/',
-  logo: <span><strong>Te Wāhi Learn</strong>
+  docsRepositoryBase: "https://github.com/et0and/learn-tewahi-next/tree/main/",
+  logo: (
+    <span>
+      <strong>Te Wāhi Learn</strong>
 
-  <style jsx>{`
-      span {
-        padding: 0.5rem 0.5rem 0.5rem 0;
-        mask-image: linear-gradient(
-          60deg,
-          black 25%,
-          rgba(0, 0, 0, 0.2) 50%,
-          black 75%
-        );
-        mask-size: 400%;
-        mask-position: 0%;
-      }
-      span:hover {
-        mask-position: 100%;
-        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
-      }
-    `}</style>
-  </span>,
-
-  banner: {
-    key: '2.0-release',
-    text: (
-      <p>
-        🚧 A new version of Learn is in development. 
-      </p>
-    )
-  },
+      <style jsx>{`
+        span {
+          padding: 0.5rem 0.5rem 0.5rem 0;
+          mask-image: linear-gradient(
+            60deg,
+            black 25%,
+            rgba(0, 0, 0, 0.2) 50%,
+            black 75%
+          );
+          mask-size: 400%;
+          mask-position: 0%;
+        }
+        span:hover {
+          mask-position: 100%;
+          transition: mask-position 1s ease, -webkit-mask-position 1s ease;
+        }
+      `}</style>
+    </span>
+  ),
 
   search: {
-    placeholder: 'Search', 
+    placeholder: "Search",
   },
 
   editLink: {
@@ -44,28 +38,34 @@ export default {
 
   toc: {
     backToTop: true,
-    title: 'On this page',
+    title: "On this page",
   },
 
   footer: {
-    text: <span>
-      {new Date().getFullYear()} © <a href="https://tewahi.com" target="_blank">Te Wāhi Auaha</a>.
-    </span>,
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{" "}
+        <a href="https://tewahi.com" target="_blank">
+          Te Wāhi Auaha
+        </a>
+        .
+      </span>
+    ),
   },
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – Te Wāhi Learn'
-    }
+      titleTemplate: "%s – Te Wāhi Learn",
+    };
   },
   feedback: {
-    content: '',
-    labels: 'feedback',
+    content: "",
+    labels: "feedback",
     useLink() {
-      const config = useConfig()
+      const config = useConfig();
       return `https://google.com/search?q=${encodeURIComponent(
         `Feedback for ${config.title}`
-      )}`
-    }
+      )}`;
+    },
   },
 
   head: (
@@ -76,4 +76,4 @@ export default {
       <link rel="icon" type="image/x-icon" href="favicon.ico" />
     </>
   ),
-}
+};
