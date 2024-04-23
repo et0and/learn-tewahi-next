64bd9513f9be5b58b92756419d1caebb95ab41db
Tom Y <gh@tomhackshaw.com>
2024-04-23 00:18:52 +0000

    Installed GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-export default function Nextra({ Component, pageProps }) {
+import { GoogleTagManager } from "@next/third-parties/google";
+
+export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <>
+      {getLayout(<Component {...pageProps} />)}
+      <GoogleTagManager gtmId="GTM-TFLHLL6N" />
+    </>
+  );
 }
